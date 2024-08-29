b28d887fa3948bcd7bc4c01006392ec810c9727c
= <jubale1328@zoho.com>
2024-08-29 10:37:26 -0700

    comma problem in css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/views/*.blade.php',
-        ''./node_modules/flowbite/**/*.js'
+        './node_modules/flowbite/**/*.js'
     ],
 
     theme: {
